28c22353e7b7c0f16e9ff8b46eb9e8e77e72ccc7
Muhammadaziz Qahhorov <124623920+QahhorovMuhammadaziz@users.noreply.github.com>
2023-02-26 19:07:21 +0500

    Delete lesson_4.py

--- lesson_4.py
+++
@@ -1,9 +0,0 @@
-# salom = 'Hello world'
-# print(salom)
-# xabar = 'Ertaga boraman'
-# print(xabar)
-
-# radius = 8
-# # pi = 3.1576
-# aylana_yuzi = pi * radius**2
-# print("Radiusi" , radius, "ga teng aylananing yuzi=", aylana_yuzi)
