2e8a52e0ed292a925e364382ac8bbc05d1a03a0b
Muhammadaziz Qahhorov <124623920+QahhorovMuhammadaziz@users.noreply.github.com>
2023-02-26 19:07:12 +0500

    Delete lesson_3.py

--- lesson_3.py
+++
@@ -1,8 +0,0 @@
-# print("Nexia", "Tico", 'Damas' 'korganlar qilar havas')
-# print(5**4)
-# print(22/4)
-# print(125*4*2)
-# print(print("Tomonlari 125 ga teng kvadratning yuzi", 125*125, "ga, perimetri", 4*125, "teng"))
-# print(36*3.14)
-# print("Tomonlari 125 ga teng kvadratning yuzi", 125*125, "ga, perimetri", 4*125, "teng")
-# print((6**2+7**2)**(1/2))
