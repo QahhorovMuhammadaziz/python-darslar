a83db544deff7ffa1361ee0d963c9f8ca3c41616
Muhammadazizqahhorov <muhammadazizqahhorov64@gmail.com>
2023-03-28 14:05:36 +0500

    profil.photo.bot

--- bot.py
+++ bot.py
@@ -7,8 +7,7 @@ bot = telebot.TeleBot('6143382079:AAF4CHF8nu1fhWxr5ej6KY1UZfNebePd7Dk')
 @bot.message_handler(commands=['start'])
 def start(message):
     photos = bot.get_user_profile_photos(message.chat.id)
-    bot.send_photo(message.chat.id,photos.photos[0][0].file_id)
-    
-    
+    bot.send_photo(message.chat.id, photos.photos[0][0].file_id)
+
+
 bot.polling()
-     
